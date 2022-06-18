064ae442e692dff519a33ab4b6f6cc41043a9a94
Giuseppe Perrotta <102922764+Giuse03@users.noreply.github.com>
2022-06-18 15:14:33 +0200

    Add files via upload

---
+++ src/main/java/net/giuse/engine/ExecuteProcess.java
@@ -0,0 +1,31 @@
+package net.giuse.engine;
+
+
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+/**
+ * This is the Engineer of all process of the modules
+ */
+public class ExecuteProcess implements Runnable {
+
+    private static final double MAX_MILLIS_PER_TICK = 30;
+    private static final int MAX_NANOS_PER_TICK = (int) (MAX_MILLIS_PER_TICK * 1E6);
+    private final Deque<Workload> workLoadDeque = new ArrayDeque<>();
+    public void addWorkload(Workload workload) {
+        workLoadDeque.add(workload);
+    }
+
+
+    @Override
+    public void run() {
+        long stopTime = System.nanoTime() + MAX_NANOS_PER_TICK;
+        Workload nextLoad;
+        while (System.nanoTime() <= stopTime && (nextLoad = this.workLoadDeque.poll()) != null) {
+            nextLoad.compute();
+        }
+    }
+}
