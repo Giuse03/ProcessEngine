b5607ff9458f74f61ee9d3193974578d95ca531e
Giuseppe Perrotta <102922764+Giuse03@users.noreply.github.com>
2022-07-12 11:25:25 +0200

    Fixed Async

--- src/main/java/net/giuse/engine/ProcessEngine.java
+++ src/main/java/net/giuse/engine/ProcessEngine.java
@@ -1,12 +1,16 @@
 package net.giuse.engine;
 
 
+import com.google.common.util.concurrent.ListeningScheduledExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ProcessEngine  {
 
@@ -26,7 +30,8 @@ public class ProcessEngine  {
     public ProcessEngine(JavaPlugin javaPlugin){
 
         Bukkit.getScheduler().scheduleAsyncRepeatingTask(javaPlugin, executeAsyncProcess,0L,0L);
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(javaPlugin, executeSyncProcess,0L,0L);
+        ListeningScheduledExecutorService scheduledExecutorService = MoreExecutors.listeningDecorator(Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors()/2));
+        CompletableFuture.runAsync(() -> scheduledExecutorService.scheduleAtFixedRate(executeAsyncProcess,20L,20L, TimeUnit.MICROSECONDS));
     }
 
 
